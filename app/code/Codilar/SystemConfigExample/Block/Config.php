ea36812cc3604eae5eadbd21dc4f1d5cb3204afe
Bibek Moktan <Bibekmoktan1222@gmail.com>
2024-04-12 12:41:45 +0530

    last change

---
+++ app/code/Codilar/SystemConfigExample/Block/Config.php
@@ -0,0 +1,29 @@
+<?php
+namespace Codilar\SystemConfigExample\Block;
+
+
+use Magento\Store\Model\ScopeInterface;
+use Codilar\SystemConfigExample\Helper\Data;
+
+
+class Config extends \Magento\Framework\View\Element\Template
+{
+    protected $helperData;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        Data $helperData
+    )
+
+{
+    $this->helperData = $helperData;
+        parent::__construct($context);
+    }
+public function getValue()
+{
+    return $this->helperData->getGeneralConfig('enable');
+}
+public function getMessage()
+{
+    return $this->helperData->getGeneralConfig('display_text');
+}
+}
---
+++ app/code/Codilar/SystemConfigExample/Helper/Data.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Codilar\SystemConfigExample\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+class Data extends AbstractHelper
+{
+
+    const XML_PATH_CONFIG = 'config/';
+
+    public function getConfigValue($field, $storeId = null)
+    {
+        return $this->scopeConfig->getValue(
+            $field, ScopeInterface::SCOPE_STORE, $storeId
+        );
+    }
+
+    public function getGeneralConfig($code, $storeId = null)
+    {
+
+        return $this->getConfigValue(self::XML_PATH_CONFIG .'general/'. $code, $storeId);
+    }
+
+}
